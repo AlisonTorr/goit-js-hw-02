092360f6c85f2187995f5daaf56c886d00a12848
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2020-12-26 19:31:15 +0300

    Add files via upload

--- js/task-1.js
+++ js/task-1.js
@@ -3,9 +3,10 @@ const numbers = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 let message;
 
 const logItems = function (array) {
-  for (let i = 1; i < pets.length; i += 1);
+  for (let i = 1; i < pets.length; i += 1) {
+    message = `${i + 1} - ${array[i]}`;
+  }
 
-  message = `${i + 1} - ${array[i]}`;
   console.log(message);
 };
 
