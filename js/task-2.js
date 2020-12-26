56e87815c1a2a6334f14829aaf8054414d136faa
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2020-12-26 22:22:06 +0300

    Add files via upload

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,9 @@
 const calculateEngravingPrice = function (message, pricePerWord) {
   const words = message.split(" ");
-  const wordsNumber = words.lenght;
-  const totalPrice = wordsNumber * pricePerWord;
+  console.log(words);
+
+  const wordsNumber = Number(words.lenght);
+  const totalPrice = Number(wordsNumber * pricePerWord);
 
   return totalPrice;
 };
