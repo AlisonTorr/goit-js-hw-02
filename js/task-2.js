71784305733e9eb07c5ce33c182ee9f88696bac6
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2020-12-26 22:16:51 +0300

    Add files via upload

--- js/task-2.js
+++ js/task-2.js
@@ -6,7 +6,24 @@ const calculateEngravingPrice = function (message, pricePerWord) {
   return totalPrice;
 };
 
-calculateEngravingPrice(
-  "Proin sociis natoque et magnis parturient montes mus",
-  10
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+);
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+);
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+);
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
 );
