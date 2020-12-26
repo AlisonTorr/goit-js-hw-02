d8822cbd37562f5b3b2508fe6f485de61c37516e
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2020-12-26 22:29:08 +0300

    Add files via upload

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,7 @@ const formatString = function (string) {
   //     string = string.pop(41);
   //     }
 
-  const newString = string.pop(40);
+  const newString = string.splice(40);
   console.log(newString);
 
   const result = string.length > 40 ? newString : string;
