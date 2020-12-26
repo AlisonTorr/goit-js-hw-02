c01f8bc81fa6de78cb4ca1775c32ed5799b02b54
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2020-12-26 22:00:39 +0300

    Add files via upload

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
 const findLongestWord = function (string) {
   const words = string.split(" ");
 
-  let longestWord = words[0].length;
+  let longestWord = words[0];
 
   for (let i = 1; i < words.length; i += 1) {
-    if (words[i].length > longestWord) {
-      longestWord = words[i].length;
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
     }
   }
 
